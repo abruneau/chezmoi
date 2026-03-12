c52da7d24b1c46e0d7b8f87be89b295de0cbcff4
abruneau <antonin.bruneau@gmail.com>
2026-03-12 11:20:23 +0100

    update 20260312

--- dot_config/sketchybar/items/widgets/brew.lua
+++ dot_config/sketchybar/items/widgets/brew.lua
@@ -1,6 +1,12 @@
 local settings = require("settings")
 local colors = require("colors")
 
+local user_home = os.getenv("HOME") or ("/Users/" .. (os.getenv("USER") or ""))
+local brew_bin = "/opt/homebrew/bin/brew"
+local brew_outdated_cmd = "/bin/zsh -lc 'export HOME=\"" ..
+user_home ..
+"\"; export PATH=\"/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin\"; HOMEBREW_NO_AUTO_UPDATE=1 " ..
+brew_bin .. " outdated --quiet 2>/dev/null'"
 
 local brew = sbar.add("item", "widgets.brew", {
     position = "right",
@@ -8,8 +14,8 @@ local brew = sbar.add("item", "widgets.brew", {
         font = {
             style = settings.font.style_map["Regular"],
             size = 19.0,
-          },
-          string = '󰏗'
+        },
+        string = '󰏗'
     },
     label = { font = { family = settings.font.numbers } },
     update_freq = 180,
@@ -28,11 +34,16 @@ local function render_bar_item(count)
         color = colors.green
         count = "􀆅"
     end
-    brew:set({ label = count, icon = { color = color } })
+    brew:set({ label = { string = tostring(count) }, icon = { color = color } })
 end
 
+local packages_rendered = false
+
 local function render_popup(packages)
-    sbar.remove('/widgets.brew.package.*/')
+    if packages_rendered then
+        sbar.remove('/widgets.brew.package.*/')
+    end
+    packages_rendered = true
     for i, package in ipairs(packages) do
         sbar.add("item", "widgets.brew.package." .. i, {
             position = "popup." .. brew.name,
@@ -48,35 +59,32 @@ local function render_popup(packages)
     end
 end
 
-brew:subscribe({"routine", "forced", "brew", "brew_update"}, function(env)
-    sbar.exec("brew update", function() end)
-    sbar.exec("brew outdated", function(brew_info)
+brew:subscribe({ "routine", "forced", "brew", "brew_update" }, function(env)
+    sbar.exec(brew_outdated_cmd, function(brew_info)
         local count = 0
         local packages = {}
         for line in brew_info:gmatch("[^\r\n]+") do
-            if line:find("^") then
-            count = count + 1
-            table.insert(packages, line)
+            if line ~= "" then
+                count = count + 1
+                table.insert(packages, line)
             end
         end
 
         render_bar_item(count)
         render_popup(packages)
-        
-        
     end)
 end)
 
 brew:subscribe("mouse.clicked", function()
     local drawing = brew:query().popup.drawing
-    brew:set( { popup = { drawing = "toggle" } })
+    brew:set({ popup = { drawing = "toggle" } })
 end)
 
 sbar.add("bracket", "widgets.brew.bracket", { brew.name }, {
     background = { color = colors.bg1 }
-  })
+})
 
 sbar.add("item", "widgets.brew.padding", {
     position = "right",
     width = settings.group_paddings
-  })
+})
